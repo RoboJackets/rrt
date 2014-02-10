6ebf7ad048bdf1a61d1df3cece2c95c28ad00f88
Justin Buchanan <justbuchanan@gmail.com>
2014-02-10 14:08:47 -0500

    added a test that actually does something

--- test/Tree.cpp
+++ test/Tree.cpp
@@ -1,9 +1,27 @@
 #include "gtest/gtest.h"
 #include <Tree.hpp>
+#include <2dplane/2dplane.hpp>
 
 using namespace RRT;
+using namespace Eigen;
 
 
 TEST(Instantiation, Tree) {
 	Tree<int> *tree = new Tree<int>();
+}
+
+TEST(Example_2dplane, Tree) {
+	Tree<Vector2f> *tree = TreeFor2dPlane(
+		100,				//	width
+		100,				//	height
+		Vector2f(90, 90),	//	goal point
+		5);					//	step size
+
+	//	give it plenty of iterations so it's not likely to fail
+	const int maxIterations = 10000;
+	tree->setMaxIterations(maxIterations);
+
+	bool success = tree->run(Vector2f(10, 10));	//	run with the given starting point
+
+	ASSERT_EQ(success, true);
 }
