bfc169f3ce8467eaac1ae3698b453cc2ad57bc86
Justin Buchanan <justbuchanan@gmail.com>
2014-02-26 15:50:19 -0500

    cleanup

--- src/2dplane/2dplane.hpp
+++ src/2dplane/2dplane.hpp
@@ -15,14 +15,17 @@ float magnitude(Eigen::Vector2f &vec);
  * configured from the given parameters.
  *
  * @param w, h The dimensions of the 2d plane.  These are used when
- *             picking random points for the Tree to move towards.
+ * picking random points for the Tree to move towards.
+ 
  * @param goal The point representing the goal that the tree is
- *             trying to find a path to
+ * trying to find a path to
+ *
  * @param step The fixed step size that the tree uses.  This is the
- *             maximum distance between nodes.
+ * maximum distance between nodes.
+ *
  * @return An RRT::Tree with its callbacks and parameters configured.
- *         You'll probably want to override the transitionValidator
- *         callback if your 2d plane has any obstacles.
+ * You'll probably want to override the transitionValidator callback
+ * if your 2d plane has any obstacles.
  */
 RRT::Tree<Eigen::Vector2f> *TreeFor2dPlane(
 	float w,
