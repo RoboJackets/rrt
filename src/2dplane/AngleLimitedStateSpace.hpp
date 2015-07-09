8bb7a5655468912316582050e0341d3dec73267c
Justin Buchanan <justbuchanan@gmail.com>
2015-07-09 01:45:04 -0400

    formatting

--- src/2dplane/AngleLimitedStateSpace.hpp
+++ src/2dplane/AngleLimitedStateSpace.hpp
@@ -5,45 +5,29 @@
 #include <Eigen/Dense>
 #include <util.hpp>
 
-
 class AngleLimitedState {
-public:
-    AngleLimitedState(const Eigen::Vector2f &pos = Eigen::Vector2f(0,0), float angle = 0, bool hasAngle = false) {
+   public:
+    AngleLimitedState(const Eigen::Vector2f &pos = Eigen::Vector2f(0, 0),
+                      float angle = 0, bool hasAngle = false) {
         setPos(pos);
         setAngle(angle);
         setHasAngle(hasAngle);
         setMaxAngleDiff(M_PI / 6.0);
     }
 
-    void setAngle(float angle) {
-        _angle = fixAngleRadians(angle);
-    }
-    float angle() const {
-        return _angle;
-    }
+    void setAngle(float angle) { _angle = fixAngleRadians(angle); }
+    float angle() const { return _angle; }
 
-    void setPos(const Eigen::Vector2f &pos) {
-        _pos = pos;
-    }
-    const Eigen::Vector2f &pos() const {
-        return _pos;
-    }
+    void setPos(const Eigen::Vector2f &pos) { _pos = pos; }
+    const Eigen::Vector2f &pos() const { return _pos; }
 
-    void setHasAngle(bool hasAngle) {
-        _hasAngle = hasAngle;
-    }
-    bool hasAngle() const {
-        return _hasAngle;
-    }
+    void setHasAngle(bool hasAngle) { _hasAngle = hasAngle; }
+    bool hasAngle() const { return _hasAngle; }
 
-    float maxAngleDiff() const {
-        return _maxAngleDiff;
-    }
-    void setMaxAngleDiff(float maxAngleDiff) {
-        _maxAngleDiff = maxAngleDiff;
-    }
+    float maxAngleDiff() const { return _maxAngleDiff; }
+    void setMaxAngleDiff(float maxAngleDiff) { _maxAngleDiff = maxAngleDiff; }
 
-private:
+   private:
     Eigen::Vector2f _pos;
     float _angle;
     float _maxAngleDiff;
@@ -52,18 +36,23 @@ private:
 
 std::ostream &operator<<(std::ostream &os, const AngleLimitedState &st);
 
-
 class AngleLimitedStateSpace : public StateSpace<AngleLimitedState> {
-public:
-    AngleLimitedStateSpace(float width, float height, float discretizedWidth, float discretizedHeight);
+   public:
+    AngleLimitedStateSpace(float width, float height, float discretizedWidth,
+                           float discretizedHeight);
 
     AngleLimitedState randomState() const;
-    AngleLimitedState intermediateState(const AngleLimitedState &source, const AngleLimitedState &target, float stepSize, bool reverse = false) const;
+    AngleLimitedState intermediateState(const AngleLimitedState &source,
+                                        const AngleLimitedState &target,
+                                        float stepSize,
+                                        bool reverse = false) const;
 
-    float distance(const AngleLimitedState &from, const AngleLimitedState &to) const;
+    float distance(const AngleLimitedState &from,
+                   const AngleLimitedState &to) const;
 
     bool stateValid(const AngleLimitedState &state) const;
-    bool transitionValid(const AngleLimitedState &from, const AngleLimitedState &to) const;
+    bool transitionValid(const AngleLimitedState &from,
+                         const AngleLimitedState &to) const;
 
     const ObstacleGrid &obstacleGrid() const;
     ObstacleGrid &obstacleGrid();
@@ -72,8 +61,10 @@ public:
     float height() const;
 
     /**
-     * @brief How much the maxAngleDiff increases for each subsequent node.  Note that this increase is limited to a max value.
-     * @details This essentially lets us increase the allowable curvature of the path slowly as the tree leads away from its startpoint.
+     * @brief How much the maxAngleDiff increases for each subsequent node.
+     * Note that this increase is limited to a max value.
+     * @details This essentially lets us increase the allowable curvature of the
+     * path slowly as the tree leads away from its startpoint.
      */
 
     // TODO: update docs, this is now a multiplier!
@@ -82,8 +73,7 @@ public:
     float maxAngleDiffDecay() const;
     void setMaxAngleDiffDecay(float decay);
 
-
-private:
+   private:
     ObstacleGrid _obstacleGrid;
     float _maxAngleDiffDecay;
 };
